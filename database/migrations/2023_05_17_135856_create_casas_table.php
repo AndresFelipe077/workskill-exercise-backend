a995beeb3b22bb12491ecdd6acf83f806421ca61
Andres Felipe Pizo Luligo <pizoluligoa@gmail.com>
2023-05-23 09:18:35 -0500

    userfactory

--- database/migrations/2023_05_17_135856_create_casas_table.php
+++ database/migrations/2023_05_17_135856_create_casas_table.php
@@ -21,8 +21,8 @@ return new class extends Migration
             $table->integer('numeroPisos');
             $table->string('descripcion');
             $table->integer('capacidad');
-            $table->foreignId('idCategoria')->references('id')->on('categorias');
-            $table->foreignId('idEstado')->references('id')->on('estados');
+            $table->foreignId('idCategoria')->references('id')->on('categorias')->onDelete('cascade');
+            $table->foreignId('idEstado')->references('id')->on('estados')->onDelete('cascade');
             $table->timestamps();
         });
     }
